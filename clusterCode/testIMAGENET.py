d4b0fbfbdb240af57d4e8eb3995befd01bd847b1
NanoNero1 <dimitri.kachler@gmail.com>
2024-07-30 04:11:40 +0200

    test

--- clusterCode/testIMAGENET.py
+++ clusterCode/testIMAGENET.py
@@ -1,10 +1,16 @@
 from datasets import load_dataset
+import time
 
 # If the dataset is gated/private, make sure you have run huggingface-cli login
+timeOne = time.time()
 dataset = load_dataset("imagenet-1k")
 
 
 print("yes this did gather imagenet!")
+
+timeTwo = time.time()
+
+print(timeTwo - timeOne)
 
 # Select the first row in the dataset
 sample = dataset[0]
