7404d47a898726fb23a0c88f7872dbb848c800a3
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-02 22:32:13 +0200

    test

---
+++ testOPT/runMedium.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=myjob         # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --gpus-per-node=a100_3g.40gb:1
+#SBATCH --time=00:30:00          # total run time limit (HH:MM:SS)
+
+module purge
+module load python/anaconda3
+
+eval "$(conda shell.bash hook)"  
+conda activate redenv 
+##python cluster_iht_agd.py
+python workIMAGENET.py
