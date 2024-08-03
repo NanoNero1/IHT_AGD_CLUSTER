ed3be0efc9a4b201816ac2f36665b9a9e88dcc6b
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-03 02:11:39 +0200

    test

--- testOPT/IHT_OPT/vanillaSGD.py
+++ testOPT/IHT_OPT/vanillaSGD.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from IHT_AGD.optimizers.baseOptimizer import myOptimizer
+from baseOptimizer import myOptimizer
 
 
 ###############################################################################################################################################################
