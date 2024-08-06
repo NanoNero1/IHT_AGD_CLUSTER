7dfb15686ce62c503fd9cf87ca7085fcfc88d4d7
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-06 20:37:06 +0200

    test

--- testOPT/train.py
+++ testOPT/train.py
@@ -188,10 +188,11 @@ def train_net(epochs, path_name, net, optimizer,run=None):
         if epoch == 0:
             run[f"trials/{optimizer.methodName}/{"epochSize"}"].append(epochStepCount)
 
-        if epoch == 20 - 1:
-            run[f"trials/{optimizer.methodName}/{"lr"}"].append(optimizer.lr)
-            optimizer.lr *= 0.1
-            run[f"trials/{optimizer.methodName}/{"lr"}"].append(optimizer.lr)
+        if epoch == 2 - 1:
+            run[f"trials/{optimizer.methodName}/{"lr"}"].append(optim.param_groups[0]['lr'])
+            for g in optimizer.param_groups:
+                 g['lr'] *= 0.1
+            run[f"trials/{optimizer.methodName}/{"lr"}"].append(optim.param_groups[0]['lr'])
 
 
 
