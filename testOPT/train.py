19af042fbf07ddee20a3dfd99dfbdcd220756a21
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-07 03:56:23 +0200

    test

--- testOPT/train.py
+++ testOPT/train.py
@@ -264,7 +264,7 @@ criterion = nn.CrossEntropyLoss()
 
 if config_optimizer == -4:
     optimizer = ihtSGD(
-      net.parameters(), beta=10.0,kappa=10.0,sparsity=0.95,
+      net.parameters(), beta=10.0,kappa=10.0,sparsity=0.99,
       momentum=config_momentum, weight_decay=config_weight_decay,device=device,model=net)
 elif config_optimizer == -3:
     optimizer = ihtAGD(
