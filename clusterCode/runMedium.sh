a835bdb5f38f0933eab771f649566717c0377a55
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-10 02:25:31 +0200

    test

--- clusterCode/runMedium.sh
+++ clusterCode/runMedium.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --gpus-per-node=a100_7g.80gb:2
-#SBATCH --time=00:30:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=60:00:00          # total run time limit (HH:MM:SS)
 
 module purge
 module load python/anaconda3
