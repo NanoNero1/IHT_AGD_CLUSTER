505ffe2cd4515d66a0fe1b3b2e8f7f02ff2a4125
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-10 02:41:08 +0200

    test

--- clusterCode/runMedium.sh
+++ clusterCode/runMedium.sh
@@ -2,6 +2,7 @@
 #SBATCH --job-name=myjob         # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=16
 #SBATCH --gpus-per-node=a100_7g.80gb:4
 #SBATCH --time=60:00:00          # total run time limit (HH:MM:SS)
 
