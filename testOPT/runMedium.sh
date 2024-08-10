2f10cdeb152543102cc91924aae2b6cbd0936b82
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-10 04:21:51 +0200

    test

--- testOPT/runMedium.sh
+++ testOPT/runMedium.sh
@@ -5,7 +5,7 @@
 ##SBATCH --mem=512G
 ##SBATCH --time=60
 ##SBATCH --cpus-per-task=16
-#SBATCH --gpus-per-node=a100_7g.80gb:4
+#SBATCH --gpus-per-node=a100_7g.80gb:2
 
 module purge
 module load python/anaconda3
