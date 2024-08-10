f1ba566d9d57567f67ecdc8c73a0608b09fe9867
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-10 04:31:59 +0200

    test

--- testOPT/runMedium.sh
+++ testOPT/runMedium.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=myjob         # create a short name for your job
 #SBATCH --nodes=1             # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --mem=512G
+#SBATCH --mem=256G
 ##SBATCH --time=60
 #SBATCH --cpus-per-task=32
 #SBATCH --gpus-per-node=a100_7g.80gb:1
