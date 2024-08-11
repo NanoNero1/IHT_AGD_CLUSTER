41f83f388f897335f2ba48c88c34d648e3b50712
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-11 05:57:34 +0200

    test

--- testOPT/loader.py
+++ testOPT/loader.py
@@ -107,7 +107,7 @@ def imagenet_loader(batch_size):
     train_set = MyDataset('train')
     test_set = MyDataset('test')
 
-    BATCH_SIZE = 512
+    BATCH_SIZE = 256
 
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True,num_workers=32)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE,num_workers=32)
