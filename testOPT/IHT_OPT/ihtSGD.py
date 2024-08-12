26d018cd588e3beedc2f378921c5b1d36b20a570
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-13 01:03:43 +0200

    test

--- testOPT/IHT_OPT/ihtSGD.py
+++ testOPT/IHT_OPT/ihtSGD.py
@@ -12,11 +12,11 @@ class ihtSGD(vanillaSGD):
     self.sparsifyInterval = sparsifyInterval
 
     # Compression, Decompression and Freezing Variables
-    self.phaseLength = 1000
+    self.phaseLength = 5000
     self.compressionRatio = 0.5
     self.freezingRatio = 0.2
-    self.warmupLength = 3000
-    self.startFineTune = 9000
+    self.warmupLength = 10000
+    self.startFineTune = 90000
 
     self.areWeCompressed = False
 
