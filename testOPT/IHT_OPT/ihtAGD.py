f220207a9177392c657eb70d5f49a52ea7a85b8b
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-12 14:36:13 +0200

    test

--- testOPT/IHT_OPT/ihtAGD.py
+++ testOPT/IHT_OPT/ihtAGD.py
@@ -77,7 +77,7 @@ class ihtAGD(vanillaAGD,ihtSGD):
 
         #Then sparsify z_t+
         ## NOTE to Dim: - you sparsify here
-        #######self.sparsify(iterate='zt')
+        self.sparsify(iterate='zt')
 
         # And then we do the actual update, NOTE: zt is actually z_t+ right now
         state['zt'] = (self.sqKappa / (self.sqKappa + 1.0) ) * state['zt'] + (1.0 / (self.sqKappa + 1.0)) * state['xt']
