126a491c04d0131ab7dc6984fbb1aa76c8437713
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-20 01:46:03 +0200

    test

--- testOPT/IHT_OPT/ihtAGD.py
+++ testOPT/IHT_OPT/ihtAGD.py
@@ -105,6 +105,7 @@ class ihtAGD(vanillaAGD,ihtSGD):
 
     # CAREFUL! this changes the parameters for the mode!
     self.getNewGrad('zt')
+    self.clipGradients()
     ######### ALTERT ######## THERE SHOULD BE self.getNewGrad('zt') above!!!
 
     with torch.no_grad():
@@ -129,6 +130,19 @@ class ihtAGD(vanillaAGD,ihtSGD):
 
     ## OFF
     #self.refreeze('zt')
+
+  def clipGradients(self,clipAmt=0.001):
+    print("I AM CLIPPING!!!!!!")
+
+    #print(len(self.param_groups))
+    #print("these are the groups")
+    #abort()
+
+    #torch.nn.utils.clip_grad_norm_(self.param_groups[0]['params'],norm_type='inf', max_norm=clipAmt)
+    torch.nn.utils.clip_grad_value_(self.param_groups[0]['params'],clip_value=clipAmt)
+    #for i 
+    #torch.clamp(self.param_groups[0]['params'],min=-1.0*clipAmt,clipAmt=1.0)
+    pass
 
   def trackMatchingMasks(self):
     concatMatchMask = torch.zeros((1)).to(self.device)
