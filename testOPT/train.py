faa727565957435db753c80169500d033734d669
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-19 20:50:47 +0200

    test

--- testOPT/train.py
+++ testOPT/train.py
@@ -249,7 +249,7 @@ def train_net(epochs, path_name, net, optimizer,run=None):
             run[f"trials/{optimizer.methodName}/{"epochSize"}"].append(epochStepCount)
 
         #if (epoch % 5) + 1 == 0:
-        if epoch in [40,50,60]:
+        if epoch in [40,50]:
             #if epoch in [6,10,14]:
             optimizer.beta *= 5.0
             run[f"trials/{optimizer.methodName}/{"lr"}"].append(optimizer.param_groups[0]['lr'])
