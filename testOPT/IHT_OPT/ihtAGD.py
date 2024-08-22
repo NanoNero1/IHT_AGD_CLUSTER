d7b6cc3b4d64f8a37288a413dad76a2e209682ec
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-22 02:30:48 +0200

    test

--- testOPT/IHT_OPT/ihtAGD.py
+++ testOPT/IHT_OPT/ihtAGD.py
@@ -97,10 +97,8 @@ class ihtAGD(vanillaAGD,ihtSGD):
         howFarAlong = ((self.iteration - self.warmupLength) % self.phaseLength) + 1
         # if self.areWeCompressed and (howFarAlong == 1):
 
-        fineTuneImprovement = False
-        if fineTuneImprovement:
-          if self.iteration >= self.startFineTune:
-            self.refreeze(iterate='zt')
+        if self.iteration >= self.startFineTune:
+          self.refreeze(iterate='zt')
         #   #pass
 
 
